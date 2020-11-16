d87de2b1c5ed79bbf4d1f47861dc3ab06fea4fe9
Laura Parker <lauramparker78@gmail.com>
2020-11-16 09:57:14 -0500

    Adding initial index.html script.js style.css files

---
+++ script.js
@@ -0,0 +1,7 @@
+       //1. append today's date (Moment.js) to the #currentCity header
+    //  & append current conditions icon to today's date
+
+    // append row & col for each: temperature, humidity, wind speed, UV index
+
+    //2. 5-Day Forecast header
+    // append current date + 1 day to #inoneday, etc
